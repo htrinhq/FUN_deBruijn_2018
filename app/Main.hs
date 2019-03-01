3a12830509b2dba449d760e99da7ce280b1f8d9f
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-03-01 15:08:54 +0100

    start working on --check flag

--- app/Main.hs
+++ app/Main.hs
@@ -11,18 +11,43 @@ import Lib
 import System.Environment
 import System.Exit
 import Numeric.Natural
+import Data.List
+import Data.Char
 
-main :: IO ()
-main = getArgs >>= parse
+--main :: IO ()
+main = do
+    getArgs >>= parse
 
-parse [n, alphabet, "--check"] = print "check" >> checkNumber n >> exit
-parse [n, alphabet, "--unique"] = print "unique" >> checkNumber n >> exit
+parse [n, alphabet, "--check"] = do
+    print "check"
+    checkNumber n
+    if allUnique alphabet == True
+        then do
+            let x = rotate 1 alphabet
+            exit
+        else usage >> exitError
+
+parse [n, alphabet, "--unique"] = do
+    print "unique"
+    checkNumber n
+    exit
 parse [n, alphabet, "--clean"] = print "clean" >> checkNumber n >> exit
-parse [n, "--check"] = checkNumber n >> exit
+parse [n, "--check"] = do
+    checkNumber n
+    exit
 parse [n, "--unique"] = checkNumber n >> exit
 parse [n, "--clean"] = checkNumber n >> exit
 parse ["-h"] = usage >> exit
 parse otherwise = usage >> exitError
+
+rotate :: Int -> String -> String
+rotate n xs = bs ++ as where (as, bs) = splitAt n xs
+
+allUnique ::(Eq a) => [a] -> Bool
+allUnique [] = True
+allUnique(x:xs)
+    | x `notElem` xs && allUnique xs = True
+    | otherwise = False
 
 usage = putStrLn "USAGE: ./deBruijn n [a] [--check|--unique|--clean]\n\n\t--check\t\tcheck if a sequence is a de Bruijn sequence\n\t--unique\tcheck if 2 sequences are distinct de Bruijn sequences\n\t--clean\t\tlist cleaning\n\tn\t\torder of the sequence\n\ta\t\talphabet [def: “01”]"
 
